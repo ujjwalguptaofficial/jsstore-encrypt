05d0414c71ee08956a0e7f24111a6e0b61a95c30
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2021-07-11 16:28:09 +0530

    release v - 1.1.1
    fix middleware not being registered because of no param in plugin

--- src/index.ts
+++ src/index.ts
@@ -19,14 +19,10 @@ export const encryptPlugin = {
     setup(connection, url) {
         const urls: any[] = [];
         if (connection.isWorker) {
-            if (!url) return;
-            if (typeof url === "string") {
-                urls.push(url as string);
-            }
-            else if (typeof url === "function") {
-                const blob = new Blob([jsstoreEncryptMiddleware.toString()], { type: "application/javascript; charset=utf-8" });
-                const blobUrl = URL.createObjectURL(blob);
-                urls.push(blobUrl);
+            if (url) {
+                if (typeof url === "string") {
+                    urls.push(url as string);
+                }
             }
             const blob = new Blob([jsstoreEncryptMiddleware.toString()], { type: "application/javascript; charset=utf-8" });
             const blobUrl = URL.createObjectURL(blob);
